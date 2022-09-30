4db6d8f30f9ac79db95e13911656dc438e772b3c
Ashutosh Kumar <ashutoshkumar6328@gmail.com>
2022-09-30 09:20:17 +0530

    Created Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import {useState, createContext} from 'react';
+import {useState, useEffect, createContext} from 'react';
 import Header from './components/Header'
 import Main from './components/Main'
 import Login from './components/Login'
@@ -6,10 +6,31 @@ import Login from './components/Login'
 const AppState = createContext();
 
 function App() {
+  const {ethereum} = window;
   const [login, setLogin] = useState(false);
+  const [address, setAddress] = useState('');
+  const [chain, setChain] = useState('');
+
+  useEffect(() => {
+    ethereum.on("chainChanged", async (chainId) => {
+      if(chainId === "0x1"){
+        setChain("Ethereum Mainnet")
+      } else if(chainId === "0x5"){
+        setChain("Goerli Testnet")
+      } else if(chainId === "0x13881"){
+        setChain("Polygon Mumbai")
+      } else{
+        setLogin(false);
+      }
+      ethereum.on("accountChanged", async (accounts) =>{
+        setAddress(accounts[0]);
+      })
+    })
+  }, [])
+  
 
   return (
-    <AppState.Provider value={{login, setLogin}}>
+    <AppState.Provider value={{login, setLogin, address, setAddress, chain, setChain}}>
     <div className="min-w-full h-screen">
       { login 
       ? 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -11,7 +11,26 @@ const Login = () => {
     try {
       await ethereum.request({method: "wallet_requestPermissions", params:[{eth_accounts: {}}]}) //Metamask syntax for requesting permission for MULTIPLE ACCOUNTS
       const accounts = await ethereum.request({method: "eth_requestAccounts"}) //Metamask syntax for requesting permission for ONE ACCOUNT
-      App.setLogin(true);
+
+      App.setAddress(accounts[0]);
+
+      const chainId = await ethereum.request({method: "eth_chainId"}) //This will get me the Current Chain Id
+      //Ropsten chain id is 0x3, Goerli is 0x5. Etherum Mainnet is 0x1. Polygon mainnet is 0x89. Polygon Mumbai is 0x13881.
+
+      if(chainId === "0x1"){
+        App.setChain("Ethereum Mainnet")
+        App.setLogin(true);
+      } else if(chainId === "0x5"){
+        App.setChain("Goerli Testnet")
+        App.setLogin(true);
+      } else if(chainId === "0x13881"){
+        App.setChain("Polygon Mumbai")
+        App.setLogin(true);
+      } else{
+        App.setLogin(false);
+        setError("Can only access with Ethereum Mainnet, Goerli, Polygon Mumbai") //You cannot use any other chain
+      }
+
     } catch (error) {
       setError(`"${error.message}"`)
     }
